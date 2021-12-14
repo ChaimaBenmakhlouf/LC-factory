a2cd5812b04d1ca6e1f0eebb43e89014a6e4a289
ChaimaBenmakhlouf <chaima.benmakhlouf@supinternet.fr>
2021-12-14 19:04:54 +0100

    Revert "ss"
    This reverts commit 89c7fb280ec0a0d4ba30f3baf9922120760ca6ea, reversing
    changes made to c219ccd4f62363a3ca744b01c6679a128591eaee.

--- config/bundles.php
+++ config/bundles.php
@@ -2,16 +2,10 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-<<<<<<< HEAD
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-=======
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
->>>>>>> 3fc952e8fd4fd0040448fae4dd58046777fab2ce
 ];
